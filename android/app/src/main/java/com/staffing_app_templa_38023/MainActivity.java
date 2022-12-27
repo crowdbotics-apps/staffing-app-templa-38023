325c922989375ed2ce0514b8a769b0858f7423b9
Crowdbotics <team@crowdbotics.com>
2022-12-27 16:02:46 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/staffing_app_templa_38023/MainActivity.java
@@ -0,0 +1,15 @@
+package com.staffing_app_templa_38023;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "staffing_app_templa_38023";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "staffing_app_templa_38023",
+  url: "https://staffing_app_templa_38023.botics.co",
+  api: "https://staffing_app_templa_38023.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
